5a47cf4d90eac65b8c30abed57c2a36efff4f995
Thierry <270067227@yoobeestudent.ac.nz>
2023-05-12 13:35:58 +1200

    added some stuff

--- Train.cpp
+++ Train.cpp
@@ -3,3 +3,29 @@ Thierry Gibbons - 270067227
 CS105-2 Section 2 Question 1 - Inheritance
 Metlink
 */
+#include "Train.h"
+
+//  Train Class
+
+//Setters
+int Train::setTrainNum()
+{
+    return trainNum;
+}
+
+TrainType Train::setTrainType()
+{
+    return trainType;
+}
+
+//Getters
+int Train::getTrainNum(int trainNum)
+{
+    return trainNum;
+}
+
+TrainType Train::getTrainType(TrainType trainType)
+{
+    return trainType;
+}
+
